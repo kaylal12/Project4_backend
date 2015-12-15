45d1978f19e9fc51999aa7b32c59ee98b3897c80
Kaylal12 <kaylalei224@gmail.com>
2015-12-15 10:38:32 -0500

    Creates profiles controller with index, show, create, update, destroy, private set_profile and private profile_params methods

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,43 @@
+class ProfilesController < ApplicationController
+  def index
+    render json: Profile.all
+  end
+
+  def show
+    render json: Profile.find(params[:id])
+  end
+
+  def create
+    @profile = current_user.create_profile(profile_params)
+
+    if @profile.save
+      render json: @profile, status: :created,
+      location: @profile
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @profile.update(profile_params)
+      head :no_content
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @profile.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :description)
+  end
+end
